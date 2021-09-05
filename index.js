b38c6d0627522eaca5c4ad7568a81ff81f4f561a
simonstockton763 <simon.stockton763@gmail.com>
2021-09-04 23:16:49 -0500

    Finished Asssignment

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const validatePassword = require('./passwordvalidation'); //import the function
 
-// Your code goes here!
+
+const validatePassword = require('./passwordvalidation'); //import the function
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+// Simon Stockton
+// CSCI 4902
+// 
+var input;
+console.log("Welcome to Simon's Password verification ");
+
+while (input != 'q') {
+
+    rl.question("Please input your password ", (input) => {
+        console.log("This password is ")
+        if (validatePassword(input)) {
+            console.log("Valid")
+        }
+        else {
+            console.log("Invalid")
+        }
+
+    })
+}
